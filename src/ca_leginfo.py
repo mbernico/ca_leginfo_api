43678a14ce4c9a7da474c54e49276f54df348f82
Mike Bernico <mike.bernico@gmail.com>
2025-03-13 18:52:28 -0700

    Added some additional status details.  Ship it.

--- src/ca_leginfo.py
+++ src/ca_leginfo.py
@@ -1,5 +1,6 @@
 """CA Leginfo API."""
 
+import dataclasses
 import requests
 from bs4 import BeautifulSoup
 import logging
@@ -7,6 +8,15 @@ import logging
 import constants
 
 logging.basicConfig(level=logging.DEBUG)
+
+@dataclasses.dataclass
+class BillStatus:
+   lead_author: str = None
+   topic: str = None
+   house_location: str = None
+   last_amended: str = None
+   committee_location: str = None
+   history: list = dataclasses.field(default_factory=list)
 
 def _get_bill_identifier(session_id: str, bill_id: str ) -> str:
   """Formats the session id and bill id so that it can be queried."""
@@ -55,3 +65,51 @@ class CALegInfoClient:
     except Exception as e:
         logging.error(f"An unexpected error occurred: {e}")
         return None
+    
+  def get_bill_status(self, bill_id:str) -> BillStatus:
+    """Returns author info, topic, last amended date, and history."""
+    bill_status_url = constants.BILL_STATUS_QUERY + _get_bill_identifier(self._session_id, bill_id)
+    logging.debug(bill_status_url)
+
+    status = BillStatus()
+
+    try:
+      response = requests.get(bill_status_url)
+      response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+      # Inspect https://leginfo.legislature.ca.gov/faces/billStatusClient.xhtml?bill_id=202320240AB2071
+      # For details if this breaks.
+      soup = BeautifulSoup(response.content, "html.parser")
+      lead_author = soup.find(id="leadAuthors")
+      topic = soup.select_one(".statusCellData #subject")
+      house_location = soup.find(id="houseLoc")
+      last_amended = soup.select_one(".statusCellData #lastAction")
+      committee_location = soup.select_one(".statusCellData #latest_commitee_location")
+      bill_history = soup.find(id="billhistory")
+
+      if lead_author:
+         status.lead_author = lead_author.text.strip()
+      if topic:
+         status.topic = topic.text.strip()
+      if house_location:
+         status.house_location = house_location.text.strip()
+      if last_amended:
+         status.last_amended = last_amended.text.strip()
+      if committee_location:
+         status.committee_location = committee_location.text.strip()
+      if bill_history:
+         tbody = bill_history.find('tbody')
+         history_rows = tbody.find_all('tr')
+         for row in history_rows:
+            cells = row.find_all('td')
+            row_dict = {'date': cells[0].text.strip(), 'action': cells[1].text.strip()}
+            status.history.append(row_dict)
+    
+      return status
+
+    except requests.exceptions.RequestException as e:
+      logging.error(f"Error fetching URL: {e}")
+      return None
+    except Exception as e:
+      logging.error(f"An unexpected error occurred: {e}")
+      return None
+
--- src/ca_leginfo_integration_test.py
+++ src/ca_leginfo_integration_test.py
@@ -3,6 +3,9 @@
 import ca_leginfo
 
 import unittest
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 class CALegInfoClientTests(unittest.TestCase):
     """Integration tests for CALegInfoClient.
@@ -19,9 +22,23 @@ class CALegInfoClientTests(unittest.TestCase):
     def test_get_bill_title(self):
         """Tests that get bill author can correctly return an author."""
         got = self._client.get_bill_title(bill_id='AB1939')
-        print(got)
         want = "AB-1939 Pupil attendance"
         self.assertIn(want, got)
+    
+    def test_bill_status(self):
+        """Tests that bill status returns properly."""
+        got = self._client.get_bill_status(bill_id='AB1939')
+        logging.debug(got)
+
+        self.assertEqual(got.lead_author, 'Maienschein (A)')
+        self.assertEqual(got.topic,
+                         'Pupil attendance: county and local school attendance review boards: pupil consultation.')
+        self.assertEqual(got.house_location, 'Secretary of State')
+        self.assertEqual(got.last_amended, '06/14/24')
+        self.assertEqual(got.committee_location, None)
+        self.assertEqual(got.history[0], 
+                         {'date': '06/14/24', 
+                          'action': 'Chaptered by Secretary of State - Chapter 13, Statutes of 2024.'})
 
 if __name__ == '__main__':
     unittest.main()
